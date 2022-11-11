b898df89efc9d0c7b43d770a4f023c9a4d9be7b6
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-11 11:33:56 -0600

    Cofigured calendar component.

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,47 @@
+import { Calendar } from 'react-big-calendar'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { addHours } from 'date-fns'
 import { Navbar } from "../components/Navbar"
+import { getMessagesES, localizer } from '../../helpers';
+
+const events = [{
+  title: 'Boss Birthday',
+  notes: 'Buy a cake',
+  start: new Date(),
+  end: addHours(new Date(), 2)
+}];
 
 export const CalendarPage = () => {
+  const eventStyleGetter = ( event, start, end, isSelected ) => {
+    console.log({event, start, end, isSelected});
+
+    const style = {
+      backgroundColor: '#447CF7',
+      opacity: 0.8,
+      color: 'white'
+    };
+
+    return {
+      style
+    }
+  };
+
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        culture="es"
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        messages={getMessagesES()}
+        eventPropGetter={ eventStyleGetter }
+      />
     </>
   )
 }
