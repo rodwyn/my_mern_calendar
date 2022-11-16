ea8fc729d61cff6e045e767df2917ee0f7c70c76
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-11-16 14:42:05 -0600

    Fixed serializableCheck issue.

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import { calendarSlice, uiSlice } from './';
 
 export const store =  configureStore({
   reducer: {
     calendar: calendarSlice.reducer,
     ui: uiSlice.reducer
-  }
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false
+  })
 });
