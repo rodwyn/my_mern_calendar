b4c7402495cf3d3010e16547207ebb8471234a7e
Rodwyn Moreno <rodwynmoreno@gmail.com>
2022-12-08 18:08:46 -0600

    Implemented new event backend.

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,9 +1,11 @@
 import { useDispatch, useSelector } from 'react-redux';
+import calendarAPI from '../api/calendarApi';
 import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from '../store';
 
 export const useCalendarStore = () => {
   const dispatch = useDispatch();
   const { events, activeEvent } = useSelector(state => state.calendar);
+  const { user } = useSelector(state => state.auth);
 
   const setActiveEvent = ( calendarEvent ) => {
     dispatch( onSetActiveEvent(calendarEvent) );
@@ -17,7 +19,9 @@ export const useCalendarStore = () => {
       dispatch(onUpdateEvent({ ...calendarEvent }));
     }  
     else {
-      dispatch( onAddNewEvent({ ...calendarEvent, _id: new Date().getTime() }));
+      const { data } = await calendarAPI.post('/events', calendarEvent);
+      console.log(data);
+      dispatch( onAddNewEvent({ ...calendarEvent, id: data.event.id, user }));
     }
   };
 
